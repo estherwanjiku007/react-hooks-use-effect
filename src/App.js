c20f4cf7883c7da78b3e244bf4dab8443f587e82
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2024-02-02 12:46:24 +0300

    aster

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-
+const [text,setText]=useState("")
+const [count,setCount]=useState(0)
 function App() {
   useEffect(() => {
     console.log("useEffect called");
@@ -7,7 +8,12 @@ function App() {
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return<div> <button onClick={(()=>
+    setCount((count)=>{count+1})
+  )}>Ive been clicked {count} times</button>
+  <input value={text}   onChange={(e)=>setText(e.target.value)}></input>
+  </div>
+;
 }
 
 export default App;
